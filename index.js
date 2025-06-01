6f861d55088d386728ac546cc1a50f43191f9d7a
sergiodealencar <sergiodealencar@gmail.com>
2025-06-01 14:52:47 -0300

    first html with javascript

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-// document.getElementById("count").innerText = 5
+// document.getElementById("count-el").innerText = 5
 
-// let count = 5
-// count = count + 1
-// console.log(count)
-
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked (log it out)
 // change the count-el in the HTML to reflect the new count
+
+// camelCase
+let countEl = document.getElementById("count-el") // pass in arguments
+
+console.log(countEl)
 
 let count = 0
 
 function increment() {
     count = count + 1
-    console.log(count)
+    countEl.innerText = count
 }
-
-
-
